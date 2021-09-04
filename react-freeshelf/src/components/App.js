836590478447b11dd4ce0d7baae4aa22f9215666
sarah-wines <sarah.lodico.wines@gmail.com>
2021-09-04 18:04:40 -0400

    renders on page, some style changes

--- react-freeshelf/src/components/App.js
+++ react-freeshelf/src/components/App.js
@@ -10,7 +10,7 @@ export const App = () => {
       <div className='flex center five'>
         <h1>Free Books</h1>
       </div>
-      <div className='flex center five three-250'>
+      <div>
         {books.map((book, idx) => {
           return <Book key={idx} title={book.title} author={book.author}url={book.url} shortDescription={book.shortDescription} coverImageUrl={book.coverImageUrl} publisher={book.publisher} publicationDate={book.publicationDate} detailedDescription={book.detailedDescription}  />
         })}
--- react-freeshelf/src/components/Book.js
+++ react-freeshelf/src/components/Book.js
@@ -14,16 +14,18 @@ export const Book = ({ title, author, url, shortDescription, coverImageUrl, publ
           <h3>{title}</h3>
         </header>
         <div className='card-content'>
-          <p><span>Author:</span>{author && author.map((item) => `${item} `)}</p>
-          <p><span>coverImageUrl:</span>{coverImageUrl && coverImageUrl.map((item) => `${item} `)}</p>
-          <p><span>shortDescription:</span> {shortDescription && shortDescription.map((item) => `${item} `)}</p>
-          <p><span>Read here:</span> {url && url.map((item) => `${item} `)}</p>
+          <p><span>By: </span>{author}</p>
+          <img src={coverImageUrl} alt="book jacket cover"/>
+          <p><span></span> {shortDescription}</p>
+          <a href= {url}>Read it here</a>
+          <div>
           {isExpanded ? <a href='#' onClick={handleExpanded}>Show Less <i class='fas fa-caret-up' /></a> : <a href='#' onClick={handleExpanded}>Show More <i class='fas fa-caret-down' /></a>}
           {isExpanded &&
-            <p><span>Publisher:</span>{publisher}</p>}
-            <p><span>Read here:</span> {url && url.map((item) => `${item} `)}</p>
+            <p><span>Publisher: </span>{publisher}</p> &&
+            <p><span>Publication Date: </span>{publicationDate}</p> &&
+            <p><span>Detailed Description: </span>{detailedDescription}</p>}
         </div>
-    
+        </div>
       </div>
     </div>
   )
